f04d2d42284e9df60602a269ff65611d7c3c14fa
jvhtec <59368337+jvhtec@users.noreply.github.com>
2025-01-10 00:43:36 +0100

    mas grande aun

--- src/components/sound/SoundTaskDialog.tsx
+++ src/components/sound/SoundTaskDialog.tsx
@@ -295,7 +295,7 @@ export const SoundTaskDialog = ({ jobId, open, onOpenChange }: SoundTaskDialogPr
               <img
                 src={createFolderIcon}
                 alt="Create Flex Folders"
-               className="h-6 w-6"
+               className="h-8 w-8"
                />
               <span className="absolute -top-8 scale-0 transition-all group-hover:scale-100 bg-secondary text-secondary-foreground text-xs px-2 py-1 rounded">
                 Create Flex Folders
