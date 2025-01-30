1391c2fec92a4d0597d88e1b61d69c0f6384eaf0
IgorReb <iluminacion2@sector-pro.com>
2025-01-30 13:40:54 +0100

    Update LightsPesosTool.tsx
    añadido item varios

--- src/pages/LightsPesosTool.tsx
+++ src/pages/LightsPesosTool.tsx
@@ -71,6 +71,7 @@ const lightsComponentDatabase = [
   { id: 58, name: 'TRUSS PROLYTE 30x30 CUBO', weight: 12 },
   { id: 59, name: 'TRUSS PRT 2.44M', weight: 45 },
   { id: 60, name: 'TRUSS PRT 3,06M', weight: 43 }
+  { id: 61, name: 'VARIOS', weight: 100 }
 ];
 
 interface TableRow {
